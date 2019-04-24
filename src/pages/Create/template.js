edc811b15232f82b3fcc387cd727621d6886f112
lovexwu <1062474646@qq.com>
2019-04-24 15:37:50 +0800

    更新新建博客页面Bug

--- src/pages/Create/template.js
+++ src/pages/Create/template.js
@@ -6,17 +6,46 @@ export default {
         title: '',
         description: '',
         content: '',
-        atIndex: false
+        atIndex: false,
+        titleNumber: 0,
+        descriptionNumber: 0,
+        contentNumber: 0
       }
     },
 
     methods:{
       onCreate(){
-        blog.createBlog({title: this.title, content: this.content, description: this.description, atIndex: this.atIndex})
+        blog.createBlog({
+          title: this.title,
+          content: this.content,
+          description: this.description,
+          atIndex: this.atIndex
+        })
         .then(res => {
           this.$message.success(res.msg)
           this.$router.push({path: `/detail/${res.data.id}`})
         })
+      },
+
+      titleTotal(){
+        let n = this.title.length
+        this.titleNumber = n
+        if(this.titleNumber === 30){
+          this.$message.warning('不可以再输入了')
+        }
+      },
+
+      descriptionTotal(){
+        let n = this.description.length
+        this.descriptionNumber = n
+        if(this.descriptionNumber === 90){
+          this.$message.warning('不可以再输入了')
+        }
+      },
+
+      contentTotal(){
+        this.contentNumber = this.content.length
+        console.log(this.content.length);
       }
     }
   }
--- src/pages/Create/template.vue
+++ src/pages/Create/template.vue
@@ -1,22 +1,42 @@
 <template>
   <div id="create">
-   <h1> 创建文章 </h1>
+   <h1>创建文章</h1>
 
    <h3>文章标题</h3>
-   <el-input type="textarea" v-model="title"></el-input> 
-   <p>限30个字</p>
+   <el-input
+     type="textarea"
+     v-model="title"
+     placeholder="输入标题"
+     maxlength="30"
+     @input="titleTotal">
+   </el-input>
+   <p>{{titleNumber}}/30</p>
 
    <h3>内容简介</h3>
-   <el-input type="textarea" v-model="description" :autosize="{ minRows: 2, maxRows: 6}"></el-input> 
-   <p>限30个字</p>
+   <el-input
+     type="textarea"
+     v-model="description"
+     :autosize="{ minRows: 2, maxRows: 6}"
+     placeholder="输入内容简介"
+     maxlength="90"
+     @input="descriptionTotal">
+   </el-input>
+   <p>{{descriptionNumber}}/90</p>
 
    <h3>文章内容</h3>
-   <el-input type="textarea" v-model="content" :autosize="{ minRows: 4, maxRows: 30}"></el-input> 
-   <p>限30个字</p>
+   <el-input
+     type="textarea"
+     v-model="content"
+     :autosize="{ minRows: 4, maxRows: 30}"
+     placeholder="输入文章内容"
+     maxlength="30"
+     @input="contentTotal">
+   </el-input>
+   <p>已输入{{contentNumber}}个字</p>
 
    <p class="switch">
     <label>是否展示到首页</label>
-    <el-switch v-model="atIndex" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+    <el-switch v-model="atIndex" active-color="#FFDD67" inactive-color="#f3002e"></el-switch>
    </p>
    <el-button @click="onCreate">确定</el-button>
   </div>
