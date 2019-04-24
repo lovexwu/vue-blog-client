312e17d052a568390d92e898aba61ec1b446966f
lovexwu <1062474646@qq.com>
2019-04-24 15:38:32 +0800

    更新编辑博客页面Bug

--- src/pages/Edit/template.js
+++ src/pages/Edit/template.js
@@ -7,7 +7,10 @@ export default {
         title: '',
         description: '',
         content: '',
-        atIndex: false
+        atIndex: false,
+        titleNumber: 0,
+        descriptionNumber: 0,
+        contentNumber:''
       }
     },
     
@@ -15,19 +18,50 @@ export default {
       this.blogId = this.$route.params.blogId
       blog.getDetail({blogId: this.blogId}).then(res =>{
         this.title = res.data.title
-        this.description = res.data.description
         this.content = res.data.content
+        this.description = res.data.description
         this.atIndex = res.data.atIndex
+
+        this.titleNumber = 0 + res.data.title.length
+        this.descriptionNumber = 0 + res.data.description.length
+        this.contentNumber = res.data.content.length
       })
     },
 
     methods:{
       onEdit(){
-        blog.updateBlog({ blogId: this.blogId }, {title: this.title, content: this.content, description: this.description, atIndex: this.atIndex})
+        blog.updateBlog({ blogId: this.blogId }, {
+          title: this.title,
+          content: this.content,
+          description: this.description,
+          atIndex: this.atIndex
+        })
         .then(res => {
+          console.log(res);
           this.$message.success(res.msg)
           this.$router.push({path: `/detail/${res.data.id}`})
         })
+      },
+
+      titleTotal(){
+        let n = this.title.length
+        this.titleNumber = n
+        if(this.titleNumber === 30){
+          this.$message.warning('不可以再输入了')
+        }
+      },
+
+      descriptionTotal(){
+        let n = this.description.length
+        this.descriptionNumber = n
+        if(this.descriptionNumber === 90){
+          this.$message.warning('不可以再输入了')
+        }
+      },
+
+      contentTotal(){
+        this.contentNumber = this.content.length
+        console.log(this.content.length);
       }
     }
   }
--- src/pages/Edit/template.vue
+++ src/pages/Edit/template.vue
@@ -2,21 +2,45 @@
   <div id="create">
    <h1> 编辑文章 </h1>
 
-   <h3>文章标题</h3>
-   <el-input type="textarea" v-model="title"></el-input> 
-   <p>限30个字</p>
+    <h3>文章标题</h3>
+    <el-input
+      type="textarea"
+      v-model="title"
+      placeholder="输入标题"
+      maxlength="30"
+      @input="titleTotal">
+    </el-input>
+    <p>{{titleNumber}}/30</p>
 
-   <h3>内容简介</h3>
-   <el-input type="textarea" v-model="description" :autosize="{ minRows: 2, maxRows: 6}"></el-input> 
-   <p>限30个字</p>
+    <h3>内容简介</h3>
+    <el-input
+      type="textarea"
+      v-model="description"
+      :autosize="{ minRows: 2, maxRows: 6}"
+      placeholder="输入内容简介"
+      maxlength="90"
+      @input="descriptionTotal">
+    </el-input>
+    <p>{{descriptionNumber}}/90</p>
 
-   <h3>文章内容</h3>
-   <el-input type="textarea" v-model="content" :autosize="{ minRows: 4, maxRows: 30}"></el-input> 
-   <p>限30个字</p>
+    <h3>文章内容</h3>
+    <el-input
+      type="textarea"
+      v-model="content"
+      :autosize="{ minRows: 4, maxRows: 30}"
+      placeholder="输入文章内容"
+      maxlength="30"
+      @input="contentTotal">
+    </el-input>
+    <p>已输入{{contentNumber}}个字</p>
 
    <p class="switch">
     <label>是否展示到首页</label>
-    <el-switch v-model="atIndex" active-color="#13ce66" inactive-color="#ff4949"></el-switch>
+    <el-switch
+      v-model="atIndex"
+      active-color="#13ce66"
+      inactive-color="#ff4949">
+    </el-switch>
    </p>
    <el-button @click="onEdit">确定</el-button>
   </div>
